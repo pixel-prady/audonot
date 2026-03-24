05ad1ae0426bf86072296b6017e250681451a56a
Pradhuman Chaudhary <121712518+pixel-prady@users.noreply.github.com>
2026-03-24 19:28:59 +0530

    Fix className for main container div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import prisma from "@/lib/db";
 const page = async () =>{
     const users = await prisma.user.findMany() ; 
     return ( 
-        <div className=" min-h-screen min-w-scheme items-center flex justify-center">
+        <div className=" min-h-screen min-w-screen items-center flex justify-center">
             {JSON.stringify(users)}
         </div>
     ) ; 
