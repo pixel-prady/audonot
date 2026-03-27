9c958a68633b161471b47fe0adeea590ffcaab0b
pixel-prady <pradhumanchaudhary580a@gmail.com>
2026-03-27 21:48:16 +0530

    reused prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import prisma from "./db";
 
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
-})
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", 
